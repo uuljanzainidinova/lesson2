1c91a78849e47265fc40a3459aed08da530801ad
210206uuljanoo <zainidinovauuljan@gmail.com>
2021-08-29 21:16:57 +0600

    update lesson2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,32 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        int age = 27;
-        int temperature = 30;
-        System.out.println(name(23, 36));
-        System.out.println(name(2, -20));
-        System.out.println(name(67, 1));
-        System.out.println(name(8, 15));
-        System.out.println(name(38, 40));
+
+        System.out.println(name(generateRandomAge(), 36));
+        System.out.println(name(generateRandomAge(), -20));
+        System.out.println(name(generateRandomAge(), 1));
+        System.out.println(name(generateRandomAge(), 15));
+        System.out.println(name(generateRandomAge(), 40));
+
+        generateRandomAge();
 
 
     }
 
-    public static String name(int age, int temperature) {
+    public static String name(int randomAge, int temperature) {
         String result = "";
-        if (20 < age && 45 < age && -20 < temperature && 30 < temperature) {
+        if (20 < randomAge && 45 > randomAge && -20 < temperature && 30 > temperature) {
 
             System.out.println("Можно идти гулять ");
 
-        } else if (age < 20 && 0 < temperature && 28 < temperature) {
+        } else if (randomAge < 20 && 0 < temperature && 28 > temperature) {
             System.out.println("Можно идти гулять");
-        } else if (45 < age && -10 < temperature && temperature < 25) {
+            System.out.println(randomAge);
+        } else if (45 < randomAge && -10 < temperature && temperature < 25) {
             System.out.println("Можно идти гулять");
         } else {
             System.out.println("Оставайтесь дома");
@@ -32,7 +36,12 @@ public class Main {
         return result;
     }
 
-
+public static int generateRandomAge() {
+    Random random = new Random();
+    int randomAge = random.nextInt(70) + 1;
+    System.out.println(randomAge);
+    return randomAge;
+}
 }
 
 
